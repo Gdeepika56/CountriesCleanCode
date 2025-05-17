5225f87a256fce7bcaa6b40895affa8836da7571
Gdeepika56 <dg1201@usnh.edu>
2025-05-17 12:27:59 -0400

    make changes

--- app/src/main/java/com/example/countriesassessment/data/model/CountriesItem.kt
+++ app/src/main/java/com/example/countriesassessment/data/model/CountriesItem.kt
@@ -1,12 +1,26 @@
 package com.example.countriesassessment.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class CountriesItem(
+    @SerializedName("capital")
     val capital: String,
+
+    @SerializedName("code")
     val code: String,
+
+    @SerializedName("currency")
     val currency: Currency,
+
+    @SerializedName("language")
+    val language: Language,
+
+    @SerializedName("name")
+    val name: String,
+
+    @SerializedName("region")
+    val region: String,
+
     val demonym: String,
     val flag: String,
-    val language: Language,
-    val name: String,
-    val region: String
 )
